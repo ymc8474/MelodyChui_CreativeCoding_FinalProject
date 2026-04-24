1ed554b2b10f5c4815dd3f25885e64f0d466910e
Melody Chui <ymc8474@nyu.edu>
2026-04-23 23:04:40 -0400

    Working Sound Update

--- melodychui_finalproject/melodychui_finalproject.js
+++ melodychui_finalproject/melodychui_finalproject.js
@@ -8,6 +8,7 @@
 // --- NOTES ---
 //14" MacbookPro Window Width: 1512px
 //14" MacbookPro Window Height: 982px
+//In order to make it work on Open Processing, I think the sound library needs to be added to the index
 
 let recording = new p5.SpeechRec(); //
 recording.onResult = detection; 
@@ -15,10 +16,11 @@ recording.start(true,true); //starts listening
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(255); //white (not in draw loop so it does not cover the drawing on each new added frame)
 }
 
 function draw() {
-    background(255); //white
+    
 }
 
 function detection() {
@@ -26,9 +28,10 @@ function detection() {
     console.log(input); //for personal reference to see whether it is detecting the right words
 
     if(input=="start"){
+        noStroke();
         fill(139, 128, 0);
         ellipse(windowWidth/2, windowHeight/2, 50) //starting flower center
-    } else if(input=="petals"){
+    } else if(input=="petals" || input=="pedals"){ //often time misoverheard
         noFill();
         stroke(0); //black
         strokeWeight(5);
@@ -37,6 +40,7 @@ function detection() {
         ellipse(windowWidth/2 - 40, windowHeight/2, 50, 30); //left petal
         ellipse(windowWidth/2 + 40, windowHeight/2, 50, 30); //right petal
     } else if(input=="red"){
+        noStroke();
         fill(255, 22, 12);
         ellipse(windowWidth/2, windowHeight/2 - 40, 30, 50); //top petal
         ellipse(windowWidth/2, windowHeight/2 + 40, 30, 50); //bottom petal
